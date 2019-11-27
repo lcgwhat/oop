00a2f7b3a8afa8eb0c94ccb764effa27305265a1
liuchg <348437207@qq.com>
2019-11-27 14:42:41 +0800

    test

--- common/config/main-prod.php
+++ common/config/main-prod.php
@@ -7,9 +7,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=mancando_dev',
-            'username' => 'mancando',
-            'password' => '123456',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=test',
+            'username' => 'root001',
+            'password' => 'Root!!2019',
             'charset' => 'utf8mb4',
             //'enableSchemaCache' => true,
         ],

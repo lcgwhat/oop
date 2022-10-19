d272b3afdd0302fce7a1c81c63227e144a1429f5
lcgwhat <348437207@qq,com>
2022-10-19 14:31:10 +0800

    user表名更改

--- common/models/User.php
+++ common/models/User.php
@@ -34,7 +34,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function tableName()
     {
-        return '{{%user}}';
+        return 'user2';
     }
 
     /**
@@ -63,7 +63,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function findIdentity($id)
     {
-        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+        return static::findOne(['id' => $id]);
     }
 
     /**
